d4942ced13fe4361aee26310e5f2a4922a7985cc
Alexander Markin <m9857925000@gmail.com>
2024-05-27 15:40:55 +0200

    fix: dependencies

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+dotenv.config({ path: "./.env.deploy" });
+
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY } = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: "api-service",
+      script: "./dist/app.js",
+    },
+  ],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      "pre-deploy": `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      "post-deploy":
+        "cd backend && pwd && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
+    },
+  },
+};
